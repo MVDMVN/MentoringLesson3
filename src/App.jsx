909b60c850bbc5c6db311ebc3c5b1e20c45df1b8
Andrey Melnichuk <edifens@gmail.com>
2021-01-21 15:04:10 +0200

    all 7 tasks finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,137 @@
-import React from 'react';
-import TodoItem from './components/TodoItem';
+import React from "react";
+import TodoItem from "./components/TodoItem";
 
-const colors = ['grey', 'red', 'blue', 'orange', 'green'];
+const colors = ["grey", "red", "blue", "orange", "green"];
 
 function App() {
+  const searchInput = React.useRef(null);
+
+  const [inputValue, setInputValue] = React.useState("");
+  const [isTaskCompleted, setIsTaskCompleted] = React.useState(false);
+  const [selectedColor, setSelectedColor] = React.useState(colors[0]);
   const [tasks, setTasks] = React.useState([
     {
       id: 1,
-      text: 'Попробовать создать ToDo',
+      text: "Попробовать создать ToDo",
+      completed: false,
+      color: "yellow",
     },
     {
       id: 2,
-      text: 'Сохранить задачи в массив стейта',
+      text: "Сохранить задачи в массив стейта",
+      completed: false,
+      color: "blue",
     },
   ]);
+
+  function addTask(selectedColor) {
+    const newTasksItem = [
+      {
+        id: tasks.length + 1,
+        text: inputValue,
+        completed: false,
+        color: selectedColor,
+      },
+    ];
+    setTasks([...tasks, ...newTasksItem]);
+    searchInput.current.focus();
+    setInputValue("");
+  }
+
+  function onEditTask(id, text) {
+    const taskEditText = prompt(
+      "А сразу подумать и нечего не менять потом?",
+      text
+    );
+    const taskId = tasks.findIndex((arg) => arg.id === id);
+    const editedTask = { ...tasks[taskId], text: taskEditText };
+    const newTasksList = [
+      ...tasks.slice(0, taskId),
+      editedTask,
+      ...tasks.slice(taskId + 1),
+    ];
+    setTasks(newTasksList);
+  }
+
+  function onRemoveTask(id) {
+    const newList = tasks.filter((item) => item.id !== id);
+    setTasks(newList);
+  }
+
+  function setCompletedTask(id) {
+    setIsTaskCompleted(true);
+    const taskId = tasks.findIndex((arg) => arg.id === id);
+    const completedTask = { ...tasks[taskId], completed: true };
+    const newTasksList = [
+      ...tasks.slice(0, taskId),
+      completedTask,
+      ...tasks.slice(taskId + 1),
+    ];
+    setTasks(newTasksList);
+  }
+
+  React.useEffect(() => {
+    searchInput.current.focus();
+    if (!tasks.length) {
+      const localStorageData = JSON.parse(localStorage.getItem("TasksList"));
+      setTasks(localStorageData);
+    }
+    localStorage.setItem("TasksList", JSON.stringify(tasks));
+  }, [tasks.length]);
+
+  const onSelectColor = (event) => {
+    setSelectedColor(event.target.id);
+  };
 
   return (
     <div className="App">
       <div className="todo">
         <h2>Список задач</h2>
         {tasks.map((obj) => (
-          <TodoItem key={obj.id} text={obj.text} />
+          <TodoItem
+            key={obj.id}
+            id={obj.id}
+            color={obj.color}
+            text={obj.text}
+            completed={obj.completed}
+            onEditTask={onEditTask}
+            onRemoveTask={onRemoveTask}
+            setIsTaskCompleted={setCompletedTask}
+          />
         ))}
         <div className="todo-input">
-          <input type="text" placeholder="Текст задачи..." />
-          <ul>
+          <input
+            ref={searchInput}
+            value={inputValue}
+            type="text"
+            placeholder="Текст задачи..."
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <div className="color-selector">
             {colors.map((color) => (
-              <li className={`todo-color ${color}`}></li>
+              // eslint-disable-next-line jsx-a11y/label-has-associated-control
+              <label className="color-selector__item" key={color}>
+                <input
+                  checked={color === selectedColor}
+                  className={color}
+                  id={color}
+                  name="color"
+                  onChange={onSelectColor}
+                  type="radio"
+                  value={color}
+                />
+                <span className={`checkmark ${color}`} />
+              </label>
             ))}
-          </ul>
+            <button
+              onClick={() => {
+                addTask(selectedColor);
+              }}
+              className="todo-add"
+            >
+              Добавить задачу
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,24 @@
-function TodoItem(props) {
+const TodoItem = ({
+  id,
+  color,
+  text,
+  completed,
+  onEditTask,
+  onRemoveTask,
+  setIsTaskCompleted,
+}) => {
   return (
     <div className="todo-item">
       <div className="todo-checkbox">
-        <label htmlFor="todo-0">
-          <input type="checkbox" id="todo-0" />
+        <label htmlFor={`todo-${id}`}>
+          <input
+            type="checkbox"
+            id={`todo-${id}`}
+            onChange={() => {
+              setIsTaskCompleted(id);
+            }}
+            checked={completed}
+          />
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <path d="M505 75a26 26 0 00-37 0L162 382 44 264a26 26 0 00-37 37l136 136c10 10 27 9 37 0l325-325c10-10 9-27 0-37z"></path>
@@ -11,18 +26,30 @@ function TodoItem(props) {
           </div>
         </label>
       </div>
-      <div className="todo-delimiter"></div>
-      <p className="todo-text">{props.text}</p>
+      <div className={`todo-delimiter ${color}`}></div>
+      <p className="todo-text">{text}</p>
       <div className="todo-actions">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 384">
-          <path d="M0 304v80h80l236-236-80-80zM378 56L328 6c-8-8-22-8-30 0l-39 39 80 80 39-39c8-8 8-22 0-30z"></path>
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
-          <path d="M63 48l32-33v-2L83 1a2 2 0 00-2 0L48 33 15 1h-2L1 13v2l32 33L1 81v2l12 12a2 2 0 002 0l33-32 33 32a2 2 0 002 0l12-12v-2L63 48z"></path>
-        </svg>
+        <div onClick={() => onEditTask(id, text)}>
+          <svg
+            className="todo-edit"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 384"
+          >
+            <path d="M0 304v80h80l236-236-80-80zM378 56L328 6c-8-8-22-8-30 0l-39 39 80 80 39-39c8-8 8-22 0-30z"></path>
+          </svg>
+        </div>
+        <div onClick={() => onRemoveTask(id)}>
+          <svg
+            className="todo-delete"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 96 96"
+          >
+            <path d="M63 48l32-33v-2L83 1a2 2 0 00-2 0L48 33 15 1h-2L1 13v2l32 33L1 81v2l12 12a2 2 0 002 0l33-32 33 32a2 2 0 002 0l12-12v-2L63 48z"></path>
+          </svg>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default TodoItem;
